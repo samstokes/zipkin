33f35ba211eb4481ab6ec1a1ed7aaef566949a8b
Sam Stokes <me@samstokes.co.uk>
2012-10-15 03:31:20 -0700

    Actually git-clone and install scribe

--- cookbooks/scribe/recipes/default.rb
+++ cookbooks/scribe/recipes/default.rb
@@ -5,6 +5,9 @@ package 'automake'
 package 'autoconf'
 package 'libevent-dev'
 package 'libboost-dev'
+package 'libboost-filesystem-dev'
+package 'libboost-system-dev'
+package 'git'
 
 
 PREFIX = '/usr/local'
@@ -53,4 +56,68 @@ bash "compile and install fb303" do
   BASH
 
   creates "#{PREFIX}/lib/libfb303.a"
+end
+
+
+bash "clone and patch scribe" do
+  cwd '/tmp'
+
+  code <<-BASH
+    set -e
+    git clone https://github.com/facebook/scribe.git
+
+    cd scribe
+
+    # https://github.com/facebook/scribe/pull/55
+    # https://github.com/powermedia/scribe/commit/6a3201f5c9b21e324e9530ff12def27cabf501d4
+    patch -p1 <<'PATCH'
+diff --git a/src/Makefile.am b/src/Makefile.am
+index 969775a..5f2e5d9 100644
+--- a/src/Makefile.am
++++ b/src/Makefile.am
+@@ -72,7 +72,7 @@ AM_CPPFLAGS += -I$(hadoop_home)/include
+ AM_CPPFLAGS += $(BOOST_CPPFLAGS)
+ AM_CPPFLAGS += $(FB_CPPFLAGS) $(DEBUG_CPPFLAGS)
+ 
+-AM_LDFLAGS = $(BOOST_LDFLAGS) $(BOOST_SYSTEM_LIB) $(BOOST_FILESYSTEM_LIB)
++AM_LDFLAGS = -Wl,--no-as-needed $(BOOST_LDFLAGS) $(BOOST_SYSTEM_LIB) $(BOOST_FILESYSTEM_LIB)
+ 
+ # Section 3 #############################################################################
+ # GENERATE BUILD RULES
+PATCH
+
+    # https://github.com/facebook/scribe/issues/36
+    patch -p1 <<'PATCH'
+diff --git a/src/file.cpp b/src/file.cpp
+index d629c2f..f6591ff 100644
+--- a/src/file.cpp
++++ b/src/file.cpp
+@@ -245,7 +245,7 @@ void StdFile::listImpl(const std::string& path, std::vector<std::string>& _retur
+       boost::filesystem::directory_iterator dir_iter(path), end_iter;
+ 
+       for ( ; dir_iter != end_iter; ++dir_iter) {
+-        _return.push_back(dir_iter->filename());
++        _return.push_back(dir_iter->path().filename().string());
+       }
+     }
+   } catch (const std::exception& e) {
+PATCH
+
+    find . -type f | xargs perl -p -i -e 's/facebook/apache/g'
+  BASH
+
+  creates '/tmp/scribe/bootstrap.sh'
+end
+
+bash "compile and install scribe" do
+  cwd '/tmp/scribe'
+
+  code <<-BASH
+    set -e
+    ./bootstrap.sh
+    make
+    sudo make install
+  BASH
+
+  not_if 'which scribed'
 end
